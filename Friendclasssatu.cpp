2eb9ad2a8ee411712730e6422a65ad0316bf9c6e
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-27 13:13:49 +0700

    menambah class siswa dengan atribut id

--- Friendclasssatu.cpp
+++ Friendclasssatu.cpp
@@ -10,10 +10,25 @@ private:
 public:
     void setName(string pNama);
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+};
+
 void orang::setName(string pNama)
 {
     nama = pNama;
 }
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
 int main()
 {
     return 0;
