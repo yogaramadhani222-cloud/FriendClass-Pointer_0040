db6780e1c6cd09fc26a5959604ec801913105ac3
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-27 13:35:22 +0700

    menambah pointer dan arrow operator

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -14,8 +14,12 @@ int main(){
     mahasiswa mhs{1};       //Object mhs
     mhs.showNim();    //member access operator
 
-    mahasiswa &refmhs = mhs; // Reference refmhs
+    mahasiswa &refmhs = mhs; //Pointer Reference refmhs
     refmhs.nim = 2; //member access operator
     refmhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //Pointer Reference pmhs
+    pMhs->nim = 3; //Arrow operator
+    pMhs->showNim();
     return 0;
 }
